f36b5341d9229a3d94ccb0fd52ef93b65d7234cd
Ezequiel Santalla <ezequielasantalla@gmail.com>
2023-10-20 22:35:58 -0300

    arreglo del .env

--- deliverableChallenges/challenge-7/src/main.js
+++ deliverableChallenges/challenge-7/src/main.js
@@ -11,33 +11,32 @@ import { __dirname } from './path.js'
 import router from './routes/index.routes.js'
 
 import initializePassport from './config/passport.js'
-import config from './config'
 
 const app = express()
 const PORT = 8080
 
 // Server
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server on PORT: ${PORT}
 http://localhost:${PORT}`)
 })
 
 // MongoDB Atlas connection
-mongoose.connect(config.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
   .then(async () => { console.log('Database connected') })
   .catch((error) => console.log("Error connecting with MongoDB ATLAS: ", error))
 
 // Middlewares App
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser(config.JWT_SECRET))
+app.use(cookieParser(process.env.JWT_SECRET))
 app.use(session({
   store: MongoStore.create({
-    mongoUrl: config.MONGO_URL,
+    mongoUrl: process.env.MONGO_URL,
     mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
     ttl: 5
   }),
-  secret: config.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true
 }))
